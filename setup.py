edf2e36b0c1073f482dc4e962574b839ceb30a29
RockFeng0 <lkf20031988@163.com>
2018-11-04 10:24:04 +0800

    update versiont to v1.2.1, support data-driven and locust client

--- setup.py
+++ setup.py
@@ -22,7 +22,55 @@ UI and Web Http automation frame for python.
 
 
 from webuidriver import __about__
-from setuptools import setup, find_packages
+import os, io, sys
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
+
+class UploadCommand(Command):
+    """ Build and publish this package.
+        Support setup.py upload. Copied from requests_html.
+    """
+
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in green color."""
+        print("\033[0;32m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        """ override
+        """
+        pass
+
+    def finalize_options(self):
+        """ override
+        """
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Publishing git tags…')
+        os.system('git tag v{0}'.format(__about__.__version__))
+        os.system('git push --tags')
+
+        sys.exit()
 
 install_requires = [
     "selenium",
@@ -38,6 +86,8 @@ setup(
         name = __about__.__title__,
         version=__about__.__version__,        
         description=__about__.__short_desc__,
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         author=__about__.__autor__,
         author_email=__about__.__author_email__,
         url=__about__.HOME_PAGE,
@@ -49,11 +99,15 @@ setup(
 #         dependency_links=dependency_links,
         extras_require={},
         entry_points={
-        'console_scripts': [
-            'wldriver=webuidriver.cli:local_main_run', # local driver
-            'wrdriver=webuidriver.cli:remote_main_run', # remote driver
-            'wrhub=webuidriver.cli:hub_main_run', # selenium grid hub
-            'wrnode=webuidriver.cli:node_main_run', # selenium grid node
-        ]
-    },
+            'console_scripts': [
+                'wldriver=webuidriver.cli:local_main_run', # local driver
+                'wrdriver=webuidriver.cli:remote_main_run', # remote driver
+                'wrhub=webuidriver.cli:hub_main_run', # selenium grid hub
+                'wrnode=webuidriver.cli:node_main_run', # selenium grid node
+            ]
+        },
+        # $ setup.py upload support.
+        cmdclass={
+            'upload': UploadCommand
+        }
     )
