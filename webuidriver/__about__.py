6d859a2e159aaeac10e3975824126ee9ded981b4
rock feng <281589463@qq.com>
2021-08-05 21:43:25 +0800

    fit: update version to 1.3.1

--- webuidriver/__about__.py
+++ webuidriver/__about__.py
@@ -3,7 +3,7 @@
 
 __title__           = "rtsf-web"
 __short_desc__      = "only for web ui test, base on rtsf"
-__version__         = "1.3.0"
+__version__         = "1.3.1"
 __autor__           = u"罗科峰"
 __author_email__    = "lkf20031988@163.com"
 __license__         = 'MIT'
