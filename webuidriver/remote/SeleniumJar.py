4370aeff1eb8be2a81520c37057248c42bca488e
rock feng <281589463@qq.com>
2021-08-05 20:16:30 +0800

    fit: pep8

--- webuidriver/remote/SeleniumJar.py
+++ webuidriver/remote/SeleniumJar.py
@@ -1,25 +1,10 @@
 # -*- encoding: utf-8 -*-
-'''
-Current module: rock4.softtest.web.SeleniumJar
 
-Rough version history:
-v1.0    Original version to use
-
-********************************************************************
-    @AUTHOR:  Administrator-Bruce Luo(罗科峰)
-    MAIL:    lkf20031988@163.com
-    RCS:      rock4.softtest.web.SeleniumJar,v 1.0 2017年2月27日
-    FROM:   2017年2月27日
-********************************************************************
-
-======================================================================
-
-UI and Web Http automation frame for python.
-
-'''
 
 import requests
-import time,subprocess
+import time
+import subprocess
+
 
 class SeleniumJar(object):    
     def __init__(self, server_jar_full_path, java_exe_full_path = "java"):
@@ -27,21 +12,25 @@ class SeleniumJar(object):
             "java_path": java_exe_full_path,
             "jar_path": server_jar_full_path,
             }
+        self._ip = "localhost"
+        self._port = 4444
+        self.command = []
+        self.__subp = None
                         
     def hub(self, port):
-        ''' java -jar selenium-server.jar -role hub -port 4444
+        """ java -jar selenium-server.jar -role hub -port 4444
         @param port:  listen port of selenium hub 
-        '''
+        """
         self._ip = "localhost"
         self._port = port 
         self.command = [self._conf["java_path"], "-jar", self._conf["jar_path"], "-port", str(port), "-role", "hub"]        
         return self
         
     def node(self,port, hub_address=("localhost", 4444)):
-        ''' java -jar selenium-server.jar -role node -port 5555 -hub http://127.0.0.1:4444/grid/register/
+        """ java -jar selenium-server.jar -role node -port 5555 -hub http://127.0.0.1:4444/grid/register/
         @param port:  listen port of selenium node
         @param hub_address: hub address which node will connect to 
-        '''
+        """
         self._ip, self._port = hub_address
         self.command = [self._conf["java_path"], "-jar", self._conf["jar_path"], "-port", str(port), "-role", "node", "-hub", "http://%s:%s/grid/register/" %(self._ip, self._port)]        
         return self
@@ -49,7 +38,7 @@ class SeleniumJar(object):
     def start_server(self):
         """start the selenium Remote Server."""        
         self.__subp = subprocess.Popen(self.command)        
-        #print("\tselenium jar pid[%s] is running." %self.__subp.pid)        
+        # print("\tselenium jar pid[%s] is running." %self.__subp.pid)
         time.sleep(2)
         
     def stop_server(self):
@@ -57,7 +46,7 @@ class SeleniumJar(object):
         :return:
         """
         self.__subp.kill()
-        #print("\tselenium jar pid[%s] is stopped." %self.__subp.pid)
+        # print("\tselenium jar pid[%s] is stopped." %self.__subp.pid)
         
     def re_start_server(self):
         """reStart the selenium Remote server"""
@@ -70,7 +59,7 @@ class SeleniumJar(object):
         """
         resp = None
         try:
-            resp = requests.get("http://%s:%s" %(self._ip, self._port))
+            resp = requests.get("http://%s:%s" % (self._ip, self._port))
 
             if resp.status_code == 200:
                 return True
@@ -78,4 +67,3 @@ class SeleniumJar(object):
                 return False
         except:
             return False
-    
